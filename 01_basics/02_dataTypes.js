10aaff38b08a633aae4fe88c379fa62d74d5c915
Raza Shah <132449925+raza29723@users.noreply.github.com>
2024-03-08 16:06:12 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict";  // treat all JS code in newer version. 
+
+// alert(3+3)   // We are using Node.js.. not browser
+
+console.log(3+
+    3
+    )   // Warning! Code readability should be high. 
+
+
+
+console.log("Hitesh")
+
+let name = "Hitesh"
+let age = 26
+let isLoggedIn = false
+
+// Data types.. and these are Primitives data types.
+// numbers=> 2
+// bigint 
+// string ""
+// boolean=> false/true
+// null (it's a stand alone value in JS) (when you intentially define such value is null)
+// undefined (for undefined value)
+// symbol (for uniqueness)
+
+
+
+// AND NOW THESE ARE REFERENCE DATA TYPES
+// objects 
+console.log(typeof "Hitesh") // string
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+
