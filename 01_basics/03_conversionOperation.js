5bc4cff1f64e08d92cd9f4a9cecd59984d884947
Raza Shah <132449925+raza29723@users.noreply.github.com>
2024-04-01 15:57:50 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = false
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let variablesInNumbers = Number(score)
+// console.log(typeof variablesInNumbers)
+// console.log(variablesInNumbers)
+
+
+// "33"  =>  33
+// "333abc"  =>  NaN
+// true => 1    and    false => 0
+
+
+let isLoggedIn = "true"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+// 1 => true   and    0 => false
+// "" => false
+// "Raza" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
