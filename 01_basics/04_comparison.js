0392995d4553e51a313959f3137547376b0eb727
Raza Shah <132449925+raza29723@users.noreply.github.com>
2024-04-25 09:16:51 +0000

    COMPARISON

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,17 @@
+// console.log(2 < 1)
+// console.log(2 > 1)
+// console.log(2 <= 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+// console.log("2" > 1)
+// console.log("2" > 1)
+
+console.log(null > 0)
+console.log(null < 0)
+console.log(null <= 0)
+console.log(null >= 0)
+console.log(null == 0)
+console.log(null === 0)
+
+/* Keep in mind, don't do these type of comparison, this condition may complex. So clean your code */
