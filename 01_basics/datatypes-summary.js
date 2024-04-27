7e06ed057038faf9d7becc2eb96d2d99388f9321
Raza Shah <132449925+raza29723@users.noreply.github.com>
2024-04-27 14:20:28 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,51 @@
+// Primitives
+// 7 types: String, Number, Boolean, Null, Undefined, Symbol, Bigint 
+// const score = 100
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+// const id = Symbol("123")
+// const anotherId = Symbol("123")
+// console.log(id === anotherId)
+
+const bigNumber = 68444687761565446765454665456984n
+
+
+// Reference (non-primitives)
+// 3 types: Array, Objects, Functions
+
+const cars = ["suzuki", "toyota", "honda", "kia", "nissan"]
+let myObj = {
+    name : "hitash",
+    age : 22
+}
+
+const myFunction =   function(){
+            console.log("Hellow World")
+   }
+ 
+   myFunction()
+
+   console.log(typeof myFunction)
+
+   /*All Components DataTypes
+   All return types in this lecture..
+   Primitives
+   score = number
+   isLoggendIn = boolean
+   outsideTemp = object
+   userEmail = undefined
+   id = symbol
+   bigNumber = bigInt
+
+
+   Reference
+   heros = object
+   myObj = object
+   myFunction = function
+
+
+
+   
+   */
+
