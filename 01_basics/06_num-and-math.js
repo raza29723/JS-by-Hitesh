aa3778f700f3e6fa0e67615e3262e91ffec6dd3a
Raza Shah <132449925+raza29723@users.noreply.github.com>
2024-05-08 16:24:44 +0000

    num-and-math

---
+++ 01_basics/06_num-and-math.js
@@ -0,0 +1,30 @@
+const score = 400
+// console.log(score)
+
+const balance = new Number(100)
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(1))
+
+const otherNumber = 1123.8966
+// console.log(otherNumber.toPrecision(4))
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// ++++++++++++++++++++++++ Maths +++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-5))
+// console.log(Math.round(5.67))
+// console.log(Math.ceil(9.1))
+// console.log(Math.floor(7.5))
+// console.log(Math.min(2, 3, 7, 9))
+// console.log(Math.max(1, 4, 5, 8))
+
+// console.log((Math.random()*10) + 1)
+// console.log(Math.floor(Math.random()*10) + 1)
+
+const min = 1
+const max = 6
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min )
